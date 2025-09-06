70f352bde92ecc203ff88a85c79726654916d1c0
unknown <nhd7noushadazeez@gmail.com>
2025-09-06 22:31:09 +0530

    scroll up button

--- koppee/src/Main/main.tsx
+++ koppee/src/Main/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './header'
 import MainHome from './home'
+import ScrollUpButton from './scrollButton'
 
 export default function Main() {
     return (
         <>
+            <ScrollUpButton />
             <Header />
-            <MainHome/>
+            <MainHome />
         </>
     )
 }
---
+++ koppee/src/Main/scrollButton.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+
+export default function ScrollUpButton() {
+    const [scrollBtn, setScrollBtn] = useState("hidden")
+
+    const handleScroll = () => {
+        const windowHeight = window.scrollY
+        setScrollBtn(
+            windowHeight > 500 ? 'hidden' : "fixed bottom-[20px] right-[20px] h-[40px] w-[40px] bg-secondary rounded-sm transition-all duration-200 z-40"
+        )
+    }
+
+    useEffect(() => {
+        handleScroll()
+        window.addEventListener('scroll', handleScroll)
+        return () => window.addEventListener('scroll', handleScroll)
+    })
+    return (
+        <a href="#home" className={scrollBtn}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="size-5 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 18.75 7.5-7.5 7.5 7.5" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 7.5-7.5 7.5 7.5" />
+            </svg>
+        </a>
+    )
+}
