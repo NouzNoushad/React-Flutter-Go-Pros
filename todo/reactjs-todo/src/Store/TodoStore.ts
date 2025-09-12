31d6acf4fac9aa24418b68560d0fda82cff93973
unknown <nhd7noushadazeez@gmail.com>
2025-09-12 21:48:44 +0530

    todo responsive

--- todo/reactjs-todo/src/Store/TodoStore.ts
+++ todo/reactjs-todo/src/Store/TodoStore.ts
@@ -3,15 +3,19 @@ import { create } from "zustand"
 type TodoStore = {
     isOpenDeleteTodo: boolean
     isOpenEditTodo: boolean
+    isOpenCreateTodo: boolean
     selectedTodoId: string | null
     setIsOpenDeleteTodo: (value: boolean, id?: string | null) => void
     setIsOpenEditTodo: (value: boolean, id?: string | null) => void
+    setIsOpenCreateTodo: (value: boolean) => void
 }
 
 export const useTodoStore = create<TodoStore>((set) => ({
     isOpenDeleteTodo: false,
     isOpenEditTodo: false,
+    isOpenCreateTodo: false,
     selectedTodoId: null,
     setIsOpenDeleteTodo: (value, id = null) => set({ isOpenDeleteTodo: value, selectedTodoId: id }),
     setIsOpenEditTodo: (value, id = null) => set({ isOpenEditTodo: value, selectedTodoId: id }),
+    setIsOpenCreateTodo: (value) => set({ isOpenCreateTodo: value })
 }))
--- todo/reactjs-todo/src/Todo/header.tsx
+++ todo/reactjs-todo/src/Todo/header.tsx
@@ -1,19 +1,50 @@
 import React from 'react'
 import { AddIcon } from '../Components/icons'
+import CustomDialog from '../Components/dailog_box'
+import FormTitle from './create_todo/pages/title'
+import FormDescription from './create_todo/pages/description'
+import FormButton from './create_todo/pages/button'
+import { useTodoStore } from '../Store/TodoStore'
+import { CreateTodoAction } from './actions/CreateTodoAction'
+import FormCheckbox from './create_todo/pages/checkbox'
 
 export default function Header() {
+    const { isOpenCreateTodo, setIsOpenCreateTodo } = useTodoStore()
+    const { handleFormSubmit } = CreateTodoAction()
     return (
-        <header className='bg-primaryColor'>
-            <div className="w-full container-lg lg:py-4 py-3">
-                <div className="flex flex-row items-center justify-between">
-                    <a href="#todo">
-                        <h1 className='md:text-xl text-lg text-whiteColor font-bold uppercase'>Todo</h1>
-                    </a>
-                    <div className="h-8 w-8 border border-whiteColor rounded-lg lg:hidden flex items-center justify-center">
-                        <AddIcon className='size-6 text-white' />
+        <>
+            <header className='bg-primaryColor'>
+                <div className="w-full container-lg lg:py-4 py-3">
+                    <div className="flex flex-row items-center justify-between">
+                        <a href="#todo">
+                            <h1 className='md:text-xl text-lg text-whiteColor font-bold uppercase'>Todo</h1>
+                        </a>
+                        <button onClick={() => setIsOpenCreateTodo(true)} className="h-8 w-8 border border-whiteColor rounded-lg lg:hidden flex items-center justify-center">
+                            <AddIcon className='size-6 text-white' />
+                        </button>
                     </div>
                 </div>
-            </div>
-        </header>
+            </header>
+            {/* Create Dialog */}
+            <CustomDialog
+                isOpen={isOpenCreateTodo}
+                onClose={() => {
+                    setIsOpenCreateTodo(false)
+                }}
+                onConfirm={undefined}
+                title="Create Todo"
+                description=""
+            >
+                <form action="" onSubmit={(e) => {
+                    handleFormSubmit(e)
+                    setIsOpenCreateTodo(false)
+                }} className='mt-8 space-y-2'>
+                    <FormTitle />
+                    <FormDescription />
+                    <FormCheckbox />
+                    <FormButton />
+                </form>
+            </CustomDialog>
+        </>
     )
 }
