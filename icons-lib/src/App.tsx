360443e24d472e908f5ae088a2b2ba27423fdbfc
unknown <nhd7noushadazeez@gmail.com>
2025-09-18 11:36:03 +0530

    icon lib initial setup

---
+++ icons-lib/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Main from './icons-site/main'
+
+function App() {
+
+    return (
+        <>
+            <Main />
+        </>
+    )
+}
+
+export default App
---
+++ icons-lib/src/icons-site/main.tsx
@@ -0,0 +1,7 @@
+import Header from "./header";
+
+export default function Main() {
+    return (
+        <Header />
+    )
+}
