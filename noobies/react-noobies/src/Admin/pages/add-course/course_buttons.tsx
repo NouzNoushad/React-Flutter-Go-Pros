ac2289c6eccf7d702414e766840d98988221f33a
unknown <nhd7noushadazeez@gmail.com>
2025-10-08 17:13:34 +0530

    minor change button

--- noobies/react-noobies/src/Admin/pages/add-course/course_buttons.tsx
+++ noobies/react-noobies/src/Admin/pages/add-course/course_buttons.tsx
@@ -10,8 +10,8 @@ export default function CourseButtons() {
 
     return (
         <div className="pt-2 flex items-center justify-end space-x-4">
-            <button type='button' onClick={handleCancel} className='px-5 py-3 rounded-md text-sm font-medium focus:outline-none foucs:ring-0 transition-colors bg-gray-100 border border-gray-300 shadow-md hover:bg-white cursor-pointer'>Cancel</button>
-            <button type='submit' className='px-5 py-3 rounded-md text-sm font-medium focus:outline-none foucs:ring-0 transition-colors bg-gray-100 border border-gray-300 shadow-md hover:bg-white cursor-pointer'>{
+            <button type='button' onClick={handleCancel} className='px-5 py-3 rounded-md text-sm font-medium focus:outline-none foucs:ring-0 transition-colors bg-gray-100 border border-gray-300 shadow-md hover:bg-white hover:border-black cursor-pointer'>Cancel</button>
+            <button type='submit' className='px-5 py-3 rounded-md text-sm font-medium focus:outline-none foucs:ring-0 transition-colors bg-gray-100 border border-gray-300 shadow-md hover:bg-white hover:border-black cursor-pointer'>{
                 'Save Changes'
             }</button>
         </div>
